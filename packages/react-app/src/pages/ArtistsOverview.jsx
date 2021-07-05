03473c26d6e7981733855319c3f5564ad8bc5926
simon <simon.r.both@gmail.com>
2021-07-05 14:20:52 +0200

    artists rarible artworks preview placeholder

--- packages/react-app/src/pages/ArtistsOverview.jsx
+++ packages/react-app/src/pages/ArtistsOverview.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import TextField from '@material-ui/core/TextField';
-import axios from 'axios';
-
+import { CircularProgress } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import ArtistOverviewCard from "../components/ArtistOverviewCard";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,14 +18,19 @@ const useStyles = makeStyles((theme) => ({
     overflow: 'hidden',
   },
   gridList: {
-    width: 500,
+    width: "100vh",
     height: "100vh",
   },
   gridListTile: {
-
   },
   SearchArtists: {
     justifyContent: 'center',
+    textAlign: 'center',
+    marginTop: "10px",
+    marginBottom: "10px"
+  },
+  centerAdornment: {
+    marginLeft: "42%" 
   },
 }));
 
@@ -72,9 +80,16 @@ export default function Artists() {
   return (
     <div className={classes.root}>
       <TextField
-        label="Search"
         className={classes.SearchArtists}
-        placeholder="Artist"
+        placeholder="Search Artist"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start" classes={{ positionStart: classes.centerAdornment}}>
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          style: { textAlign: "center" },
+        }}
         fullWidth
         value={searchInput || ""}
         color="primary"
@@ -93,7 +108,7 @@ export default function Artists() {
                 <ArtistOverviewCard artist={artist} />
               </GridListTile>
             ))
-          : ""}
+          : <CircularProgress />}
       </GridList>
     </div>
   );
