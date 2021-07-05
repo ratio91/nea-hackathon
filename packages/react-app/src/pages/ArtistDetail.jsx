ad3932d4a0a62101797f672af6c2a8f3b58bcd82
simon <simon.r.both@gmail.com>
2021-07-05 17:50:29 +0200

    small artist profile layout fix

--- packages/react-app/src/pages/ArtistDetail.jsx
+++ packages/react-app/src/pages/ArtistDetail.jsx
@@ -4,9 +4,26 @@ import { useProfileApi } from "../hooks";
 import { useParams } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { Alert } from "@material-ui/lab";
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 import ArtistsArtworkOverview from "../components/ArtistsArtworkOverview";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
 export default function ArtistDetail({ neaContract }) {
+  const classes = useStyles();
   const { id: address } = useParams();
   const { profile, isLoading } = useProfileApi(address);
   const [amount, setAmount] = useState(0);
@@ -41,22 +58,28 @@ export default function ArtistDetail({ neaContract }) {
   };
 
   return (
-    <>
-      <div className="Box">
-        <h1>{profile.name}</h1>
-        <p>Biography: {profile.biography}</p>
-        <p>Date of birth: {dateOfBirth}</p>
-        <p>Art style: {profile.artStyle}</p>
-        <TextField label="Amount" type="number" onChange={onSetAmount} />
-        <Button onClick={onInvest} variant="contained" color="primary">
-          Invest
-        </Button>
-        {error && <Alert severity="error">{error}</Alert>}
-        {message && <Alert severity="success">{message}</Alert>}
+    <Grid container className={classes.root}
+      direction="column"
+      justify="center"
+      alignItems="center">
+      <Grid item>
+        <Card className="Box">
+          <h1>{profile.name}</h1>
+          <p>Biography: {profile.biography}</p>
+          <p>Date of birth: {dateOfBirth}</p>
+          <p>Art style: {profile.artStyle}</p>
+          <TextField label="Amount" type="number" onChange={onSetAmount} />
+          <Button onClick={onInvest} variant="contained" color="primary">
+            Invest
+          </Button>
+          {error && <Alert severity="error">{error}</Alert>}
+          {message && <Alert severity="success">{message}</Alert>}
 
-      </div>
-
-      <ArtistsArtworkOverview />
-    </>
+        </Card>
+      </Grid>
+      <Grid item>
+        <ArtistsArtworkOverview />
+      </Grid>
+    </Grid>
   );
 }
