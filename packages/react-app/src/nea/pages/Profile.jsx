a20f978ef0df68203a58cc15ac88f2c3133d19e0
jooddang <jooddang@gmail.com>
2021-10-06 11:00:39 -0700

    add json metadata on profile page

--- packages/react-app/src/nea/pages/Profile.jsx
+++ packages/react-app/src/nea/pages/Profile.jsx
@@ -4,7 +4,12 @@ import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/picker
 import DateFnsUtils from "@date-io/date-fns";
 import { useForm, Controller } from "react-hook-form";
 import { useProfileApi } from "../hooks";
+import ReactJson from "react-json-view";
 import { Alert } from "@material-ui/lab";
+
+const ipfsAPI = require("ipfs-http-client");
+const ipfs = ipfsAPI({ host: "ipfs.infura.io", port: "5001", protocol: "https" });
+
 
 export default function Profile({ address, neaFactoryContract, neaContract, tx, signer, gasPrice, web3Modal, loadWeb3Modal}) {
   const { register, reset, control, handleSubmit, setValue } = useForm();
@@ -14,6 +19,9 @@ export default function Profile({ address, neaFactoryContract, neaContract, tx, 
   const [message, setMessage] = useState("");
   const { profile, updateProfile, isLoading } = useProfileApi(address, reset, setDate);
   const contract = profile && profile.neaContractAddress ? neaContract.attach(profile.neaContractAddress) : null;
+  let [yourJSON, setYourJSON] = useState({});
+  const [sending, setSending] = useState();
+  const [ipfsHash, setIpfsHash] = useState();
 
   const onChangeDate = data => {
     setDate(data);
@@ -73,6 +81,23 @@ export default function Profile({ address, neaFactoryContract, neaContract, tx, 
 
   if (web3Modal) {
     if (web3Modal.cachedProvider) {
+      yourJSON = {
+        description: profile.artStyle,
+        external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
+        image: "https://austingriffith.com/images/paintings/buffalo.jpg",
+        name: profile.name,
+        attributes: [
+          {
+            trait_type: "BackgroundColor",
+            value: "green",
+          },
+          {
+            trait_type: "Eyes",
+            value: "googly",
+          },
+        ],
+      };
+
       return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <div className="Box">
@@ -132,6 +157,49 @@ export default function Profile({ address, neaFactoryContract, neaContract, tx, 
                     <Button onClick={onDistribute} variant="contained" color="primary">
                       Distribute
                     </Button>
+
+                    <div style={{ paddingTop: 32, width: 740, margin: "auto", textAlign: "left" }}>
+                      <ReactJson
+                        style={{ padding: 8 }}
+                        src={yourJSON}
+                        theme="pop"
+                        enableClipboard={false}
+                        onEdit={(edit, a) => {
+                          setYourJSON(edit.updated_src);
+                        }}
+                        onAdd={(add, a) => {
+                          setYourJSON(add.updated_src);
+                        }}
+                        onDelete={(del, a) => {
+                          setYourJSON(del.updated_src);
+                        }}
+                      />
+                    </div>
+
+                    <Button
+                      style={{ margin: 8 }}
+                      loading={sending}
+                      size="large"
+                      shape="round"
+                      type="primary"
+                      onClick={async () => {
+                        console.log("UPLOADING...", yourJSON);
+                        setSending(true);
+                        setIpfsHash();
+                        const result = await ipfs.add(JSON.stringify(yourJSON)); // addToIPFS(JSON.stringify(yourJSON))
+                        if (result && result.path) {
+                          setIpfsHash(result.path);
+                        }
+                        setSending(false);
+                        console.log("RESULT:", result);
+                      }}
+                    >
+                      Upload to IPFS
+                    </Button>
+
+                    <div style={{ padding: 16, paddingBottom: 150 }}>
+                      <a target='_blank' rel="noopener" href={`https://ipfs.io/ipfs/${ipfsHash}`}>{ipfsHash}</a>
+                    </div>
                   </>
                 )}
                 {error && <Alert severity="error">{error}</Alert>}
